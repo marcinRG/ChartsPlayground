b03e773a05a4e5855163e756f2538f483d004a07
marcinRG <marcin.guszpit@gmail.com>
2020-04-28 07:26:50 +0200

    changes

--- src/toBeDeleted/other.ts
+++
@@ -1,8 +0,0 @@
-export function func(x: number): string {
-    return x + '';
-}
-
-export const func2 = (x: number, y: number): string => {
-    const sum = x + y;
-    return <string>(sum + '');
-};
