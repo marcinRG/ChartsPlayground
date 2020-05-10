90278485b65caad605d1b5b4053205d7edc68821
marcinRG <marcin.guszpit@gmail.com>
2020-05-10 23:06:11 +0200

    changes

--- src/components/Pages/InfoPage/Info.page.tsx
+++ src/components/Pages/InfoPage/Info.page.tsx
@@ -7,48 +7,43 @@ export function InfoPage() {
             <div className="info-page">
                 <div className="selected-wrapper">
                     <h2 className="section-title">Info</h2>
-                    <div>
-                        Simple react page.
+                    <div className="content">
+                        <h3 className="sub-title">About</h3>
+                        <p>Simple react page.</p>
 
-                        This site is using following technologies:
-                        Typescript
-                        Webpack
-                        d3.js
-                        svg
-                        React + context api
+                        <h3 className="sub-title">This site is using following technologies:</h3>
+                        <ul className="item-list">
+                            <li>Typescript</li>
+                            <li>Webpack</li>
+                            <li>d3.js</li>
+                            <li>svg</li>
+                            <li>React + context api</li>
+                        </ul>
 
-                        images:
+                        <h3 className="sub-title">Images</h3>
 
-                        Photo by Andrea Piacquadio from Pexels
-                        https://www.pexels.com/pl-pl/zdjecie/ludzie-kobieta-biuro-dziewczyna-3808822/
+                        <ul className="item-list">
+                            <li><a href="https://www.pexels.com/pl-pl/zdjecie/blond-dziewczyna-folder-kobieta-1587009/">Photo by Moose Photos from Pexels</a></li>
+                            <li><a href="https://www.pexels.com/pl-pl/zdjecie/osoba-rece-ludzie-kobieta-3787818/">Photo by bongkarn thanyakij from Pexels</a></li>
+                            <li><a href="https://www.pexels.com/photo/view-of-high-rise-buildings-during-day-time-302769/">Photo by Pixbay</a></li>
+                            <li><a href="https://www.pexels.com/pl-pl/zdjecie/biurka-biuro-coworking-firma-7070/">Photo by Startup Stock Photos from Pexels</a></li>
+                        </ul>
 
+                        <h3 className="sub-title">Page template</h3>
 
-                        Photo by Moose Photos from Pexels
-                        https://www.pexels.com/pl-pl/zdjecie/blond-dziewczyna-folder-kobieta-1587009/
+                        <ul className="item-list">
+                            <li><a href="https://psdfreebies.com/psd/agency-creative-website-template-free-psd/">Agency Creative Website Template</a></li>
+                            <li><a href="https://drive.google.com/drive/folders/0Bw2hu70L5Ye_RVNaYU5nclNjeUk?fbclid=IwAR0u35DNuAcPsO9LMIcLa10r8QSgoAchdLNN3_aG0vGe1JFu2A_z-FRU3Z0">backup</a></li>
+                        </ul>
 
+                        <h3 className="sub-title">Other resources</h3>
 
-                        Photo by bongkarn thanyakij from Pexels
-                        https://www.pexels.com/pl-pl/zdjecie/osoba-rece-ludzie-kobieta-3787818/
+                        <ul className="item-list">
+                            <li><a href="https://www.toptal.com/react/react-context-api">React context api</a></li>
+                            <li><a href="https://github.com/typescript-cheatsheets/react-typescript-cheatsheet">React typescript cheet sheet</a></li>
+                            <li><a href="https://www.d3-graph-gallery.com/index.html">D3 chart gallery</a></li>
+                        </ul>
 
-
-                        Photo by Pixbay
-                        https://www.pexels.com/photo/view-of-high-rise-buildings-during-day-time-302769/
-
-
-                        Photo by Startup Stock Photos from Pexels
-                        https://www.pexels.com/pl-pl/zdjecie/biurka-biuro-coworking-firma-7070/
-
-
-                        Page template:
-                        https://psdfreebies.com/psd/agency-creative-website-template-free-psd/
-                        backup
-                        https://drive.google.com/drive/folders/0Bw2hu70L5Ye_RVNaYU5nclNjeUk?fbclid=IwAR0u35DNuAcPsO9LMIcLa10r8QSgoAchdLNN3_aG0vGe1JFu2A_z-FRU3Z0
-
-                        Other resources:
-
-                        React context api: https://www.toptal.com/react/react-context-api
-                        React typescript cheet sheet https://github.com/typescript-cheatsheets/react-typescript-cheatsheet
-                        D3 chart gallery https://www.d3-graph-gallery.com/index.html
                     </div>
                 </div>
             </div>
